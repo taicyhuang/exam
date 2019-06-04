b10ccbfa2e966a6ff43980c4083973d7fb6fa6a1
Webber <taicy.huang@gmail.com>
2019-06-05 07:59:46 +0800

    Update modify-form1w-add-ajax.php

--- modify-form1w-add-ajax.php
+++ modify-form1w-add-ajax.php
@@ -2,10 +2,10 @@
 
 
 header("Content-Type: text/html; charset=utf-8");
-$myhost     = "172.16.44.8";
-$myuser     = "root";
-$mypassword = "seat";
-$mydatabase = "tra_y2019";
+$myhost     = $myhost;
+$myuser     = $myuser;
+$mypassword = $mypassword;
+$mydatabase = $mydb;
 
 try{
     $pdo=new PDO(
